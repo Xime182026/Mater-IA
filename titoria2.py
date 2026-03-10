40c8dfc530aeaa9abe3ced1e1700b8ce1712f878
Xime182026 <ximguzman16@gmail.com>
2026-03-09 19:52:03 -0500

    cambio3

--- titoria2.py
+++ titoria2.py
@@ -10,4 +10,6 @@ else:
 # Puedes usar comentarios para explicar tu código o para dejar notas para ti mismo o para otros programadores.
 # También puedes usar comentarios para desactivar temporalmente una línea de código sin eliminarla. 
  print("hola jero")
- 
+ #el git gurdo con el comando git add titoria2.py
+ #el git commit con el comando git commit -m "Agregando titoria2.py"
+ #el git push con el comando git push origin main
