25dda840c697cf2c41fcc2713d237d29a24d197b
Xime182026 <ximguzman16@gmail.com>
2026-03-09 19:42:35 -0500

    cambio2

--- titoria2.py
+++ titoria2.py
@@ -1,3 +1,13 @@
 print("hola mundo")
 nombre = input("¿Cuál es tu nombre? ")
 print("¡Hola, " + nombre + "! Bienvenido a Python.")
+edad = int(input("¿Cuántos años tienes? "))
+if edad < 18:
+    print("Eres menor de edad.")
+else:
+    print("Eres mayor de edad.")    
+# Esto es un comentario en Python
+# Puedes usar comentarios para explicar tu código o para dejar notas para ti mismo o para otros programadores.
+# También puedes usar comentarios para desactivar temporalmente una línea de código sin eliminarla. 
+ print("hola jero")
+ 
